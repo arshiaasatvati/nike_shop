20c51630f63a5618a69d380f00e0ecbba9cc1afd
Arshia Satvati <satvatiarshia@gmail.com>
2025-08-29 01:55:10 +0330

    Added Theme and Colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_shop/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,27 +11,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const TextStyle defaultTextStyle = TextStyle(fontFamily: 'Shabnam');
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontFamily: 'Shabnam',
+            fontWeight: FontWeight.bold,
+            color: LightThemeColors.primaryTextColor,
+          ),
+          labelMedium: defaultTextStyle.apply(
+            color: LightThemeColors.secondaryTextColor,
+          ),
+          headlineMedium: defaultTextStyle.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+
+          titleLarge: defaultTextStyle.copyWith(fontWeight: FontWeight.bold),
+        ),
+        colorScheme: ColorScheme.light(
+          primary: LightThemeColors.primaryColor,
+          onPrimary: Colors.white,
+          secondary: LightThemeColors.secondaryColor,
+          onSecondary: Colors.white,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Directionality(
+        textDirection: TextDirection.rtl,
+        child: const MyHomePage(title: 'فروشگاه نایک'),
+      ),
     );
   }
 }
@@ -80,10 +90,12 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Colors.white,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        // titleTextStyle: Theme.of(context).textTheme.headlineSmall,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -104,7 +116,13 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+            const Text('شما دکمه را اینقدر فشار داده اید'),
+            Text(
+              'شما دکمه را اینقدر فشار داده اید',
+              style: Theme.of(
+                context,
+              ).textTheme.labelMedium!.copyWith(fontSize: 12),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
@@ -114,6 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
+        backgroundColor: Theme.of(context).colorScheme.secondary,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LightThemeColors {
+  static const Color primaryColor = Color(0xff217cf3);
+  static const Color secondaryColor = Color(0xff262a35);
+  static const Color primaryTextColor = Color(0xff262a35);
+  static const Color secondaryTextColor = Color(0xffb3b6be);
+}
